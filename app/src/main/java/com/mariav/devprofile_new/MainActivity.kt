efd5da30bf899f5dd9b2eb6334798ad27f5e269e
sheidin <ilya.sheidin@gmail.com>
2019-02-22 13:51:03 +0200

    First setup of Android grid

---
+++ app/src/main/java/com/mariav/devprofile_new/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mariav.devprofile_new
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
