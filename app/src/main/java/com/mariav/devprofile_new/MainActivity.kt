ffe4bc1aa03af63a162da59e0b14402212e15c60
sheidin <ilya.sheidin@gmail.com>
2019-03-01 14:45:43 +0200

    Make avatar rounded by using bitMap

--- app/src/main/java/com/mariav/devprofile_new/MainActivity.kt
+++ app/src/main/java/com/mariav/devprofile_new/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.mariav.devprofile_new
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bitmap = BitmapFactory.decodeResource(resources,R.mipmap.avatar_macc_lay)
+        val rounded = RoundedBitmapDrawableFactory.create(resources,bitmap);
+        rounded.cornerRadius = 100f;
+        Avatar.setImageDrawable(rounded)
     }
 }
